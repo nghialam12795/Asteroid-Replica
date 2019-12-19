b8366161d04468bc413e3f7a755fff568d9fd440
Nghia Lam <nghialam12795@gmail.com>
2019-12-19 22:56:34 +0700

    - Create window and fix SDL2 import for mac

--- src/constant.cpp
+++ src/constant.cpp
@@ -2,4 +2,6 @@
 
 struct Constant {
   static const int8_t kGameFPS = 60;
+  static const int kWidth = 1200;
+  static const int kHeight = 720;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,76 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 using namespace std;
 
-int main() {
-  cout << "Hello, World!!" << endl;
+#include "constant.cpp"
+
+// The window we will be rendering to
+SDL_Window* window = nullptr;
+
+// The surface contain by the window
+SDL_Surface* screenSurface = nullptr;
+
+
+bool init() {
+  // Init flag
+  bool success = true;
+
+  // Init SDL
+  if(SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    cout << "SDL Failed to Initialize !!! \n SDL ERRORS: " << SDL_GetError() << endl;
+    success = false;
+  }
+  else
+  {
+    window = SDL_CreateWindow("SDL !!!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Constant::kWidth, Constant::kHeight, SDL_WINDOW_SHOWN);
+    SDL_SetWindowTitle(window, "Pixel Plot");
+    if (window == nullptr)
+    {
+      cout << "Cannot create window !!! SDL ERRORS: " << SDL_GetError() << endl;
+      success = false;
+    }
+    else
+    {
+      // Get window surface
+      screenSurface = SDL_GetWindowSurface(window);
+    }
+  }
+
+  return success;
+}
+
+void close() {
+  // Destroy the window
+  SDL_DestroyWindow(window);
+  window = nullptr;
+
+  // Quit SDL Subsystem
+  SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+  // Start up SDL and create the window
+  if (!init()) {
+    cout << "Cannot create the window !!" << endl;
+  }
+  // Main loop flag
+  bool quit = false;
+
+  // Event Handler
+  SDL_Event event;
+
+  while (!quit) {
+    while (SDL_PollEvent(&event) != 0) {
+      if (event.type == SDL_QUIT) {
+        quit = true;
+      }
+    }
+  }
+
+  // Free resources and close SDL
+  close();
+
   return 0;
 }
 
